462216de6e4c1433d334a202c301a92070953dcf
yohan78 <yohan.billon@yahoo.fr>
2019-02-01 12:01:40 +0100

    creation script2.js

---
+++ script2.js
@@ -0,0 +1,14 @@
+CTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Se souvenir d'un visiteur</title>
+    </head>
+    <body>
+        Bonjour <strong id="visit_name">anonyme</strong>,<br />
+        Ceci est votre visite N&deg; <strong id="nb_visits">1</strong>.<br />
+        <input type="button" value="Supprimer mes informations" onclick="scookie('visit_name','',-1);scookie('nb_visits','',-1);" /><br />
+        M&ecirc;me si vous rafraichissez la page ou que vous fermer votre navigateur, le cookie restera enregistr&eacute;e jusqu'&agrave; ce que vous le supprimiez ou jusqu'&agrave; ce qu'il expire.
+        <script type="text/javascript">
+
+
